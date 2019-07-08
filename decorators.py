a980b241e93c3eb2ea6909dd57341b237c418ad1
mojimi <ppgab.mail@gmail.com>
2019-07-08 13:03:59 -0300

    Create decorators.py

---
+++ decorators.py
@@ -0,0 +1,53 @@
+def email_in_session_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if not "email" in session:
+            return redirect(url_for("login"))
+        else:
+            return f(*args, **kwargs)
+
+    return wrapper
+
+def load_project(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        project = projects.get_by_id(kwargs['project_id'])
+        if project:
+            g.project = project
+            return f(*args, **kwargs)
+    return wrapper
+
+def load_user(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        user = users.get_by_email(session['email'])
+        if user:
+            g.user = user
+            return f(*args, **kwargs)
+    return wrapper
+
+def user_in_project_required(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        user_in_project = users.get_project_by_mail_by_id(session['email'], kwargs['project_id'])
+        if user_in_project:
+            g.user_in_project = user_in_project
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('user_home'))
+    return wrapper
+
+#Adds a "can" function to global
+def checks_permission(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        #TODO: Could probably gain a couple milliseconds by storing as dict instead of list
+        def can(*actions):
+            role_permissions = g.user_in_project.role_permissions
+            for action in actions:
+                if action not in role_permissions:
+                    return False
+            return True
+        g.can = can
+        return f(*args, **kwargs)
+    return wrapper
